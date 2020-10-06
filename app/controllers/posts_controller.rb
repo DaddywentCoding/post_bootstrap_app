8ce4d5f0fa5eea214e4d7a2c3853826eabfab33b
daddywentcoding <1month5mankasego@gmail.com>
2020-10-06 22:57:14 +0900

    編集機能を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,19 +12,28 @@ class PostsController < ApplicationController
   end
 
   def create
-    post = Post.create!(posts_params)
+    post = Post.create!(post_params)
     redirect_to post
   end
 
   def destroy
     post = Post.find(params[:id])
     post.destroy!
-    redirect_to post
+    redirect_to posts_path
   end
 
+  def edit    
+    @post = Post.find(params[:id])    
+  end
+
+  def update    
+    post = Post.find(params[:id])
+    post.update!(post_params)
+    redirect_to post    
+  end
   private
 
-  def posts_params
+  def post_params
     params.require(:post).permit(:title, :content)
   end  
 end
